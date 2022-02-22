9d2e5b10f9a63eddb5e5ad2c163a877637ed9a46
SushiWaUmai <eugene.bb1@gmail.com>
2022-02-22 01:14:18 +0100

    feat: :package: add glfw and glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
 int main(void)
 {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+    gladLoadGL();
+
+    glfwTerminate();
+
+    return 0;
 }
