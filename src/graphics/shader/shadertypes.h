de04fe4e13d97ae17529ce412b56c5e07b29b94f
SushiWaUmai <eugene.bb1@gmail.com>
2022-03-30 23:17:54 +0200

    feat: :sparkles: add geometry shader

--- src/graphics/shader/shadertypes.h
+++ src/graphics/shader/shadertypes.h
@@ -4,7 +4,9 @@ namespace yes
 {
     template class ShaderSource<GL_VERTEX_SHADER>;
     template class ShaderSource<GL_FRAGMENT_SHADER>;
+    template class ShaderSource<GL_GEOMETRY_SHADER>;
 
     typedef ShaderSource<GL_VERTEX_SHADER> VertexShader;
     typedef ShaderSource<GL_FRAGMENT_SHADER> FragmentShader;
+    typedef ShaderSource<GL_GEOMETRY_SHADER> GeometryShader;
 } // namespace yes
