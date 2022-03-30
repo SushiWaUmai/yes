1d3294b3ebe3ee169be64396df82e39fdc649485
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2022-03-31 00:19:40 +0200

    feat: :sparkles: add window hints for core profile, resizble, forward compat and scale to monitor

--- src/core/window.cpp
+++ src/core/window.cpp
@@ -5,6 +5,12 @@ namespace yes
     void Window::Init(const char *title, int width, int height)
     {
         glfwInit();
+
+        glfwWindowHint(GLFW_SCALE_TO_MONITOR, GLFW_TRUE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
         window = glfwCreateWindow(width, height, title, NULL, NULL);
         glfwMakeContextCurrent(window);
     }
