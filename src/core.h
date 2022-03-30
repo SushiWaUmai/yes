1b33dee6aed5b326f718d634ed4bd28f2a48c268
SushiWaUmai <eugene.bb1@gmail.com>
2022-03-30 22:51:09 +0200

    refactor: :truck: move all shader sources to one folder

--- src/core.h
+++ src/core.h
@@ -11,14 +11,17 @@
 #include "utils/fileloader.h"
 
 #include "graphics/texture.h"
-#include "graphics/shadertypes.h"
+
+#include "graphics/shader/shaderdata.h"
+#include "graphics/shader/shadersource.h"
+#include "graphics/shader/shader.h"
+
 #include "graphics/buffer/buffer.h"
 #include "graphics/buffer/vertexbuffer.h"
 #include "graphics/buffer/indexbuffer.h"
 #include "graphics/buffer/vertexarray.h"
 #include "graphics/buffer/framebuffer.h"
-#include "graphics/shadersource.h"
-#include "graphics/shader.h"
+
 #include "graphics/renderer.h"
 
 #include "core/window.h"
